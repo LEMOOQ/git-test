8afc6f5be4d24a638d25810f63ee1a001c64034f
krystal <904867168@qq.com>
2018-12-08 20:54:17 +0800

    添加宣义

--- src/com/itheima/demo01/First.java
+++ src/com/itheima/demo01/First.java
@@ -4,6 +4,7 @@ public class First {
 
     public static void main(String[] args) {
         System.out.println("hello krystal");
+        System.out.println("hello 吴宣仪");
     }
 
     public void test(){
